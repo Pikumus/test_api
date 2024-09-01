05c39fdb7a96e0367076b4eac235f89affda7df0
Pikumus <104292005+Pikumus@users.noreply.github.com>
2024-09-01 09:48:29 +0300

    rename method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import connection from './db';
 import connectToDatabase from "./db";
+
 const querystring = require('querystring');
 const url = require('url');
 
@@ -9,7 +10,7 @@ const port = 3000;
 app.use(express.json());
 
 
-app.get('/api/universal', async (req, res) => {
+app.get('/api/products', async (req, res) => {
     let parsedUrl = url.parse(req.url);
     let parsedQs = querystring.parse(parsedUrl.query);
     let query = parsedQs['query'];
@@ -17,20 +18,11 @@ app.get('/api/universal', async (req, res) => {
     try {
         const rows = await db.all(query);
         res.json(rows);
-    }
-    catch (err) {
+    } catch (err) {
         res.status(500).send(err);
-    }
-    finally {
+    } finally {
         await db.close();
     }
-    // connectToDatabase.query(query, (err, results) => {
-    //     if (err) {
-    //         res.status(500).send(err);
-    //         return;
-    //     }
-    //     res.json(results);
-    // })
 });
 
 // Пример API для получения данных
